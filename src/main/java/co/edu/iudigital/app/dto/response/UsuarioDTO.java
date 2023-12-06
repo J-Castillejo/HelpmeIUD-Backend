39464bed207040a054f08f0af7e27c03ec9b833c
J-Castillejo <jhan.castillejo@est.iudigital.edu.co>
2023-12-06 18:59:11 -0500

    Add DelitoDTO and UsuarioDTO classes

---
+++ src/main/java/co/edu/iudigital/app/dto/response/UsuarioDTO.java
@@ -0,0 +1,34 @@
+package co.edu.iudigital.app.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UsuarioDTO {
+
+    Long id;
+
+    String nombre;
+
+    String apellido;
+
+    String userName;
+
+    Boolean redSocial;
+
+    LocalDate fechaNacimiento;
+
+    String imagen;
+
+    boolean disponibilidad;
+
+    List<String> roles;
+
+}
