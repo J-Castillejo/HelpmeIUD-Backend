b3e3eec9e5154d4f78cac3557c04458bab00c220
J-Castillejo <jhan.castillejo@est.iudigital.edu.co>
2023-12-06 19:00:29 -0500

    Add CasoDTO class

---
+++ src/main/java/co/edu/iudigital/app/dto/CasoDTO.java
@@ -0,0 +1,43 @@
+package co.edu.iudigital.app.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CasoDTO {
+
+    Long id;
+
+    @JsonProperty("fecha_hora")
+    LocalDateTime fechaHora;
+
+    Float latitud;
+
+    Float longitud;
+
+    Float altitud;
+
+    String descripcion;
+
+    @JsonProperty("is_visible")
+    Boolean isVisible;
+
+    @JsonProperty("url_map")
+    String urlMap;
+
+    @JsonProperty("rmi_url")
+    String rmiUrl;
+
+    @JsonProperty("usuario_id")
+    Long usuarioId;
+
+    @JsonProperty("delito_id")
+    Long delitoId;
+}
