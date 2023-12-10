530646802c2cbe9b8aa7d56777587744fd2c1899
J-Castillejo <jhan.castillejo@est.iudigital.edu.co>
2023-12-10 17:24:55 -0500

    Add new fields to UsuarioDTORequest

--- src/main/java/co/edu/iudigital/app/dto/request/UsuarioDTORequest.java
+++ src/main/java/co/edu/iudigital/app/dto/request/UsuarioDTORequest.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -43,5 +44,7 @@ public class UsuarioDTORequest {
 
     String imagen;
 
-    boolean disponibilidad;
+    boolean enabled;
+
+    List<Long> roles;
 }
